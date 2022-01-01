7c9c606fdb93f711acce54ba5c5ebfc3589bfb67
Ipsone <antonioernestodossantos@gmail.com>
2022-01-01 20:50:46 -0300

    Lista

--- Aula5/Program.cs
+++ Aula5/Program.cs
@@ -6,7 +6,12 @@ namespace Aula5 {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine(  );
-            List<int> lista = new List<int>(); 
+            List<int> lista = new List<int>();
+            lista.Add(1 * 2);
+            lista.Add(1 * 3);
+            foreach(int obj in lista) {
+                Console.WriteLine(obj);
+            }
         }
     }
 }
