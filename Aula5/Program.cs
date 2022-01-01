bfd97a83b0610f41411cd94842a2edab20d8bf62
Ipsone <antonioernestodossantos@gmail.com>
2022-01-01 20:47:23 -0300

    Primeiro teste

---
+++ Aula5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula5 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(  );
+            List<int> lista = new List<int>(); 
+        }
+    }
+}
